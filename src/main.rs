748b167ae0d901b8ed698021f8247bc1e4eaa5fc
LCmaster <luizcv89@gmail.com>
2022-12-04 12:35:10 -0300

    updating the base code to account for changes

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod eth_wallet;
 
 fn main() {
     let (sec_key, pub_key) = eth_wallet::generate_keypair();
-    println!("Hello, world!");
+    println!("secret key: {}", &sec_key.to_string());
+    println!("public key: {}", &pub_key.to_string());
 }
