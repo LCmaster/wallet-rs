9c987803b88e488b8b99bface2db0ef07dd61c24
LCmaster <luizcv89@gmail.com>
2022-12-04 12:47:43 -0300

    Generating the public address from the public key

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod eth_wallet;
 
 fn main() {
     let (sec_key, pub_key) = eth_wallet::generate_keypair();
+    let pub_address = eth_wallet::public_key_address(&pub_key);
+
     println!("secret key: {}", &sec_key.to_string());
     println!("public key: {}", &pub_key.to_string());
+    println!("public address: {:?}", pub_address);
 }
