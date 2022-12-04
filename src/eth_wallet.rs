c547a02a1182475aa2274b100a592b04fa53382f
LCmaster <luizcv89@gmail.com>
2022-12-04 13:46:16 -0300

    Added very basic file saving & loading capabilities

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -1,9 +1,54 @@
+use std::{
+    fs::OpenOptions,
+    io::{BufReader, BufWriter},
+};
+
+use anyhow::{Ok, Result};
 use secp256k1::{
     rand::{rngs, SeedableRng},
     PublicKey, SecretKey,
 };
+use serde::{Deserialize, Serialize};
 use tiny_keccak::keccak256;
 use web3::types::Address;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EthWallet {
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String,
+}
+
+impl EthWallet {
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let public_address = public_key_address(&public_key);
+        let address = format!("{:?}", public_address);
+        EthWallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            public_address: address,
+        }
+    }
+
+    pub fn save_to_file(&self) -> Result<()> {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("wallet.json")?;
+        let buf_writer = BufWriter::new(file);
+        serde_json::to_writer_pretty(buf_writer, self)?;
+
+        Ok(())
+    }
+
+    pub fn load_from_file() -> Result<Self> {
+        let file = OpenOptions::new().read(true).open("wallet.json")?;
+        let buf_reader = BufReader::new(file);
+
+        let wallet: Self = serde_json::from_reader(buf_reader)?;
+        Ok(wallet)
+    }
+}
 
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
+use anyhow::{Ok, Result};
+
+use crate::eth_wallet::EthWallet;
+
 mod eth_wallet;
 
-fn main() {
+fn main() -> Result<()> {
     let (sec_key, pub_key) = eth_wallet::generate_keypair();
     let pub_address = eth_wallet::public_key_address(&pub_key);
+    let eth_wallet = EthWallet::new(&sec_key, &pub_key);
 
     println!("secret key: {}", &sec_key.to_string());
     println!("public key: {}", &pub_key.to_string());
     println!("public address: {:?}", pub_address);
+    println!("");
+    println!("Ethereum Walet");
+    println!("{:?}", &eth_wallet);
+
+    eth_wallet.save_to_file();
+
+    let loaded_wallet = eth_wallet::EthWallet::load_from_file();
+
+    println!("");
+    println!("Loaded Ethereum Walet");
+    println!("{:?}", &loaded_wallet);
+
+    Ok(())
 }
